e22689b9a66e595d3c73e5a6feb961abbe864bc3
andres santana <andres.santana@gmail.com>
2014-07-19 13:51:04 -0700

    Adding some wait samples

---
+++ concurrent_programming/src/concurrent_programming/SynchronizedInstanceMethods.java
@@ -0,0 +1,23 @@
+package concurrent_programming;
+
+public class SynchronizedInstanceMethods {
+
+	public static synchronized void method() {
+		System.out.println("SynchronizedInstanceMethods.method() from " + Thread.currentThread().getName());
+	}
+
+	public static void main(String[] args) {
+		// this is using java 8 lambdas
+		new Thread(() -> {
+			try {
+				Thread.sleep(2500);
+			} catch (Exception e) {
+			}
+			SynchronizedInstanceMethods.method();
+		}, "t1").start();
+
+		new Thread(() -> {
+			SynchronizedInstanceMethods.method();
+		}, "t2").start();
+	}
+}
